c79d3295ca695f791a835d6957c8e57d8bf1712e
Gabriel Carlsson <gabriel-carlsson@hotmail.se>
2025-05-29 02:56:43 +0200

    Updated auto_client.py

--- pi_zero/scripts/auto_client.py
+++ pi_zero/scripts/auto_client.py
@@ -1,29 +1,35 @@
 import socket
+import time
 
 def start_client():
-    # Create a socket connection for the client
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     # Define the TCP-servers IP-adress and portnumber
     server_ip = "192.168.1.128" 
     server_port = 5000 
-    
-    # Try to connect to the server
-    try:
-        client_socket.connect((server_ip, server_port))
+    while True:
+        try:
+            # Create a socket connection for the client
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        # Send a message automatically when the TCP-server connects
-        client_socket.sendall(b"Hello from auto_client!")
+            # Bind the port and the ip-adress  
+            client_socket.connect((server_ip, server_port))
         
-        # Receive data from TCP
-        data = client_socket.recv(1024)
-        print(f"TCP: Hello Client!, your message was received, thank you.")
+            # Send a message automatically when the TCP-server connects
+            client_socket.sendall(b"auto_client responding!")
+        
+            # Receive data from TCP
+            data = client_socket.recv(1024)
+            print(f"TCP: Hello Client!, your message was received, thank you.")           
+            break 
 
-    except ConnectionRefusedError:   
-        print("Couldn't connect to the server.")
+        except ConnectionRefusedError:   
+            print("Couldn't connect to the server. Retrying in 3s...")
+            time.sleep(3)
     
-    finally:
-        client_socket.close()
+        finally:
+            client_socket.close()
 
 if __name__ == "__main__":
+    time.sleep(1.5) # wait a little before running again
     start_client() 
+pi@raspberrypi:~/RnD/Gabbemannen00/pi_zero/scripts $ 
